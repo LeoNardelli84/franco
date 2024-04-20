b136e0888b5cc9ffe8a99754d0fba092073f6618
LeoNardelli84 <siblings.distribuidora@gmail.com>
2024-04-20 11:02:24 +0100

    testeo reproductor, no carga la cancion

--- js/index.js
+++ js/index.js
@@ -1,8 +1,33 @@
+function init(){
+    var audio = $("#audio");
+    var playlist = $(".playlist");
+    var tracks = playlist.getElementsByTagName('a');
+    audio.volume = 0.30;
+    audio.
+    audio.play();
+
+};
+
+
 $("a").click(function(e){
-    e.preventDefault();
-    $("a").css('background-color', 'white');
-    $("a").children('img').attr('src', 'assets/fotos/portada.png')
     
+    $("a").css('background-color', 'white');
+    $("a").children('img').attr('src', 'assets/fotos/portada.png');
     $(this).css('background-color', '#ff8e04');
     $(this).children('img').attr('src','assets/fotos/music.gif');
+    
+    var cancion = $(this).find(":nth-child(3)").text();
+    var url = $(this).attr('href');
+    var audio = $('#audio');
+    e.preventDefault();
+    audio.volume = 0.3;
+    //audio.src(url);
+    
+    
+
+    
+    $("#nombre_tema").html(cancion);
+    
+    
+    console.log(url);
 });
