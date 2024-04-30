4338953c23b956cd9d6f2c93c2a0d1d077ce39a4
LeoNardelli84 <siblings.distribuidora@gmail.com>
2024-04-30 15:45:57 +0100

    probando reproductor casero y el btn pause

--- js/index.js
+++ js/index.js
@@ -1,33 +1,35 @@
-function init(){
-    var audio = $("#audio");
-    var playlist = $(".playlist");
-    var tracks = playlist.getElementsByTagName('a');
-    audio.volume = 0.30;
-    audio.
-    audio.play();
 
-};
+$("#btn-stop").hide();
+let playlist = $('#playlist');
+let canciones = playlist.children('a');
+let audio = new Audio();
 
+//sconsole.log(temas[2]);
+//function changeTrack(url, audio){
+//    $("#reproductor").attr('src', url).appendTo(audio);
 
 $("a").click(function(e){
     
+    var cancion = $(this).find(":nth-child(3)").text();
+    var url = $(this).attr('href');
+    audio.volume = 0.3;
+    audio.src = url;
+    
+    audio.load();
+    audio.play();
+
+    $("#nombre_tema").html(cancion);
+    $("#btn-stop").show();
     $("a").css('background-color', 'white');
     $("a").children('img').attr('src', 'assets/fotos/portada.png');
     $(this).css('background-color', '#ff8e04');
     $(this).children('img').attr('src','assets/fotos/music.gif');
-    
-    var cancion = $(this).find(":nth-child(3)").text();
-    var url = $(this).attr('href');
-    var audio = $('#audio');
     e.preventDefault();
-    audio.volume = 0.3;
-    //audio.src(url);
-    
-    
+});
 
-    
-    $("#nombre_tema").html(cancion);
-    
-    
-    console.log(url);
+$("#btn-stop").click(function(){
+   audio.pause();
+   audio.currentTime = 0;
+   $("a").children('img').attr('src', 'assets/fotos/portada.png');
+   
 });
